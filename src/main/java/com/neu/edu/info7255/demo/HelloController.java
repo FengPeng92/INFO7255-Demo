7814d0aa7b264a67faed5749a6bf516ee7c69d54
FengPeng92 <peng.fen@northeastern.edu>
2022-02-15 10:02:47 -0500

    first commit

---
+++ src/main/java/com/neu/edu/info7255/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.neu.edu.info7255.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
---
+++ src/test/java/com/neu/edu/info7255/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neu.edu.info7255.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
