a82a751bd39631a7bccd584c2d4e39e4f89075d1
FengPeng92 <peng.fen@northeastern.edu>
2022-02-17 00:22:40 -0500

    etag configuration

--- src/main/java/com/neu/edu/info7255/demo/controller/HelloController.java
+++ src/main/java/com/neu/edu/info7255/demo/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.neu.edu.info7255.demo.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,11 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestHeader HttpHeaders headers) {
+
+        return headers.getIfNoneMatch().get(0);
     }
 }
--- src/main/java/com/neu/edu/info7255/demo/controller/PlanController.java
+++ src/main/java/com/neu/edu/info7255/demo/controller/PlanController.java
@@ -5,47 +5,78 @@ import org.everit.json.schema.ValidationException;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import redis.clients.jedis.Jedis;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class PlanController {
 
     Jedis jedis = new Jedis();
-    @PostMapping("/plan")
-    public Map<String, String> addPlan(@RequestBody(required = false) String planString) {
-        Map<String, String> map = new HashMap<>();
+    // etag value -> plan id
+    Map<String, String> eTags = new HashMap<>();
+
+    @PostMapping("/addPlan")
+    public ResponseEntity addPlan(@RequestBody(required = false) String planString,  @RequestHeader HttpHeaders headers) {
         JSONObject planJson = new JSONObject(planString);
         JSONObject jsonSchema = new JSONObject(new JSONTokener(PlanController.class.getResourceAsStream("/schema.json")));
         Schema schema = SchemaLoader.load(jsonSchema);
         try {
             schema.validate(planJson);
-
-            map.put("objectId", planJson.getString("objectId"));
-            jedis.set(planJson.getString("objectId"), planString);
         } catch (ValidationException e) {
-            System.out.println(e.getMessage());
+            return new ResponseEntity("Invalid data", HttpStatus.BAD_REQUEST);
         }
 
-        return map;
+        String etag = headers.getETag();
+        System.out.println(etag);
+        if (eTags.containsKey(etag)) {
+            return new ResponseEntity("This plan is in the store", HttpStatus.NOT_MODIFIED);
+        } else {
+            eTags.put(etag, planString);
+            jedis.set(planJson.getString("objectId"), planString);
+            JSONObject responseBody = new JSONObject();
+            responseBody.put("objectId", planJson.getString("objectId"));
+            return new ResponseEntity(responseBody.toMap(), headers, HttpStatus.CREATED);
+        }
     }
 
 
     @GetMapping("/getPlan/{id}")
-    public String getPlan(@PathVariable(required = true) String id) {
+    public ResponseEntity getPlan(@PathVariable(required = true) String id, @RequestHeader HttpHeaders headers) {
 
-        String planString = jedis.get(id);
-        return planString;
+        List<String> list = headers.getIfNoneMatch();
+        System.out.println(list.size());
+        if (!list.isEmpty()) {
+            String requestETag = list.get(0);
+
+            if (eTags.containsKey(requestETag)) {
+                System.out.println("Not modified");
+                return new ResponseEntity("This plan is not modified", HttpStatus.NOT_MODIFIED);
+            } else {
+                eTags.put(requestETag, id);
+                return new ResponseEntity(jedis.get(id), HttpStatus.ACCEPTED);
+            }
+        } else {
+            return new ResponseEntity(jedis.get(id), HttpStatus.ACCEPTED);
+        }
 
     }
 
     @DeleteMapping("/deletePlan/{id}")
-    public void deletePlan(@PathVariable(required = true) String id) {
-        System.out.println(id);
+    public ResponseEntity deletePlan(@PathVariable(required = true) String id) {
+
+        for (String key : eTags.keySet()) {
+            if (eTags.get(key).equals(id)) {
+                eTags.remove(key);
+            }
+        }
         jedis.del(id);
+
+        return new ResponseEntity("Delete Successfully", HttpStatus.ACCEPTED);
     }
 
 }
